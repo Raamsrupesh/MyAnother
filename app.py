a15dc5c6038bd899973ae65f34590ff53e0551ab
Raamsrupesh <yerrarupesh756@gmail.com>
2026-01-06 18:37:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import pandas as pd 
 import sqlite3 
+# import uuid
 conn = sqlite3.connect('todotask.db')
 cur = conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS todotask(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, status VARCHAR(2) NOT NULL, task VARCHAR(2000) NOT NULL);")
-conn.commit()
+tab = st.text_input("Enter the table name: ")
+if st.button("GO"):
+    if tab != "":
+        cur.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
+        tables = cur.fetchall()
+        st.write(tables)
+        cur.execute(f"CREATE TABLE IF NOT EXISTS todotask{tab}(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, status VARCHAR(2) NOT NULL, task VARCHAR(2000) NOT NULL);")
+        conn.commit()
+    else:
+        st.warning("It's EMPTY!")
 st.title("To do List: ")
-
 st.write("---")
 
-df = pd.read_sql("SELECT * FROM todotask;", con=conn)
+df = pd.read_sql(f"SELECT * FROM todotask{tab};", con=conn)
 try:
     for j,i in df.iterrows():
         a, b = st.columns([12,1])
@@ -19,18 +27,18 @@ try:
             elif i['status'] == "✅":
                 choice = st.checkbox(f"{i['task']}", value=True)
             if choice:
-                cur.execute("UPDATE todotask SET status = '✅' WHERE id = ?;", (i['id'],))
+                cur.execute(f"UPDATE todotask{tab} SET status = '✅' WHERE id = ?;", (i['id'],))
                 conn.commit()
             elif not choice:
-                cur.execute("UPDATE todotask SET status = '❌' WHERE id = ?;", (i['id'],))
+                cur.execute(f"UPDATE todotask{tab} SET status = '❌' WHERE id = ?;", (i['id'],))
                 conn.commit()
         with b:
             if st.button("🗑️", key = f"{j}{i}key"):
-                cur.execute("DELETE FROM todotask WHERE id = ?;", (i['id'],))
+                cur.execute(f"DELETE FROM todotask{tab} WHERE id = ?;", (i['id'],))
                 conn.commit()
                 st.rerun()
         if st.button("Clear ALL"):
-            cur.execute("DELETE FROM todotask")
+            cur.execute(f"DELETE FROM todotask{tab}")
             conn.commit()
             st.rerun()
 except st.errors.StreamlitDuplicateElementId:
@@ -41,8 +49,9 @@ with st.form(f"TASK", clear_on_submit=True):
     abc = st.text_input("Enter Task: ")
     if st.form_submit_button("ADD"):
         if abc != "":
-            cur.execute(f"INSERT INTO todotask(status, task) VALUES(?, ?);", ('❌',abc))
+            cur.execute(f"INSERT INTO todotask{tab}(status, task) VALUES(?, ?);", ('❌',abc))
             conn.commit()
+
 
 
 
